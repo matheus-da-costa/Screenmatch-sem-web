2789865ee6ddfd7135c9b52c1dd92c49467bb92e
Matheus Da Costa Sousa <sousa.mcs@dpf.gov.br>
2024-02-27 18:10:20 -0300

    Inicio do projeto spring

---
+++ src/main/java/br/com/alura/screenmatch/service/ConverteDados.java
@@ -0,0 +1,22 @@
+package br.com.alura.screenmatch.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.com.alura.screenmatch.model.DadosSerie;
+
+public class ConverteDados implements IConverteDados {
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public <T> T obterDados(String json, Class<T> classe) {
+        try {
+            return mapper.readValue(json, classe);
+        } catch (JsonProcessingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
