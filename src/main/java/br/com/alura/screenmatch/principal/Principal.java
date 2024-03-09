4027a37f3f5205005436a31f7d062bb3ab8849d1
Matheus Da Costa Sousa <sousa.mcs@dpf.gov.br>
2024-03-08 23:22:55 -0300

    feat - Atribuindo informações somente do titulo no projeto

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoApi;
@@ -35,5 +36,14 @@ public class Principal {
 		}
 
 		temporadas.forEach(System.out::println);
+
+        // for(int i = 0; i < dados.totalTemporadas(); i++) {
+        //     List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodio();
+        //     for(int j = 0; j < episodiosTemporada.size(); j++) {
+        //         System.out.println(episodiosTemporada.get(j).titulo());
+        //     }
+        // }
+
+        temporadas.forEach(t -> t.episodio().forEach(e -> System.out.println(e.titulo())));
     }
 }
